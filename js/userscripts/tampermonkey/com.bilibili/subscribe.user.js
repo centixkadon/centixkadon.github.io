81db22f9f3dacbaa5e08a4c9e2f88d37279b167e
centixkadon <centixkadon@gmail.com>
2019-04-29 14:39:36 +0800

    Update bilibili_subscribe

--- js/userscripts/tampermonkey/com.bilibili/subscribe.user.js
+++ js/userscripts/tampermonkey/com.bilibili/subscribe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili_subscribe
 // @namespace    https://github.com/centixkadon/centixkadon.github.io/tree/master/js/userscripts/tampermonkey
-// @version      0.2.2
+// @version      0.2.3
 // @description  Highlight subscribe.
 // @author       centixkadon
 // @match        https://space.bilibili.com/*/bangumi
@@ -63,7 +63,7 @@
       });
 
       $follow.each(function () {
-        this.__vue__._data.pageSize = 50;
+        this.__vue__.pageSize = 50;
         this.__vue__.pageChanged();
       });
     }, 33);
